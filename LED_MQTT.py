fe811b2efbfb915b9322a81aa8011becd695425d
Aratiganesh123 <arati2310@gmail.com>
2021-05-01 17:55:21 +0530

    added MQTT LED code

---
+++ LED_MQTT.py
@@ -0,0 +1,42 @@
+import RPi.GPIO as GPIO
+import time
+import paho.mqtt.client as mqtt
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18,GPIO.OUT)
+GPIO.output(18,GPIO.LOW)
+
+#GPIO.output(18,GPIO.LOW)
+
+
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("wheels/sensor")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    #print(msg.topic+" "+str(msg.payload))
+    print(msg.payload)
+    print ("LED ON")
+    GPIO.output(18,GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(18,GPIO.LOW)
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("test.mosquitto.org", 1883, 60)
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_forever()
+
